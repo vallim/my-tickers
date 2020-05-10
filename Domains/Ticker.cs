78c856bb52a939e0233d7b3e02082ad15d82902e
Fabricio Vallim <fmvallim@gmail.com>
2020-05-10 15:47:58 -0300

    Configures database connection using dapper Creates method to get all tickers from database

--- Domains/Ticker.cs
+++ Domains/Ticker.cs
@@ -2,6 +2,8 @@ namespace my_tickers.Domains
 {
     public class Ticker
     {
-        
+        public string Code { get; set; }
+        public string Type { get; set; }
+        public string TradingName { get; set; }
     }
 }
--- Repositories/TickersRepository.cs
+++ Repositories/TickersRepository.cs
@@ -1,19 +1,32 @@
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 using my_tickers.Domains;
 using my_tickers.Repositories.Contracts;
+using Npgsql;
 
 namespace my_tickers.Repositories
 {
     public class TickersRepository : ITickersRepository
     {
+        private IDbConnection _database;
+        
+        public TickersRepository(IConfiguration configuration)
+        {
+            _database = new NpgsqlConnection(configuration.GetSection("DbConnectionString").Value);
+        }
+        
         public void Save(Ticker ticker)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public List<Ticker> GetAll()
         {
-            throw new System.NotImplementedException();
+            return _database.Query<Ticker>("SELECT * FROM tickers").ToList();
         }
     }
 }
