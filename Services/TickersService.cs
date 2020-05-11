9793fcdfb466461e6398995412e5330eef14c116
Fabricio Vallim <fmvallim@gmail.com>
2020-05-10 22:20:38 -0300

    Adjusts service to use repository

--- Services/TickersService.cs
+++ Services/TickersService.cs
@@ -1,14 +1,22 @@
 using System.Collections.Generic;
 using my_tickers.Domains;
+using my_tickers.Repositories.Contracts;
 using my_tickers.Services.Contracts;
 
 namespace my_tickers.Services
 {
     public class TickersService : ITickersService
     {
+        private readonly ITickersRepository _tickersRepository;
+
+        public TickersService(ITickersRepository tickersRepository)
+        {
+            _tickersRepository = tickersRepository;
+        }
+
         public List<Ticker> GetAll()
         {
-            return new List<Ticker>();
+            return _tickersRepository.GetAll();
         }
     }
 }
